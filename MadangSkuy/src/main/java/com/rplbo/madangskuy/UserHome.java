518be8c3e1cc7f4d44f8f323521d8bbfb90b2331
Dedeo3 <yoannes.deo@ti.ukdw.ac.id>
2023-05-12 23:09:42 +0700

    update controller home page

--- MadangSkuy/src/main/java/com/rplbo/madangskuy/UserHome.java
+++ MadangSkuy/src/main/java/com/rplbo/madangskuy/UserHome.java
@@ -1,5 +1,28 @@
 package com.rplbo.madangskuy;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class UserHome {
+    Parent root;
+    Scene scene;
+    Stage stage;
+
+    public void buttonkategori(ActionEvent event) throws IOException {
+        Parent kategoriPage = FXMLLoader.load(getClass().getResource("user-ButtonKategori.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(kategoriPage));
+        stage.show();
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+    }
+
+
 
 }
