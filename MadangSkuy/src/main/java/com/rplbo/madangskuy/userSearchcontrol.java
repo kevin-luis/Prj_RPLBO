d4c4a3ae9c76e6fb426198ac47d8a4db0b52bbdd
Dedeo3 <yoannes.deo@ti.ukdw.ac.id>
2023-05-12 23:11:55 +0700

    add controller search page

---
+++ MadangSkuy/src/main/java/com/rplbo/madangskuy/userSearchcontrol.java
@@ -0,0 +1,15 @@
+package com.rplbo.madangskuy;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class userSearchcontrol {
+
+
+
+}
