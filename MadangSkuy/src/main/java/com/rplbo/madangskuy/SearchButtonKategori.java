f8f07597596f23680938c9ee87c1202a55a56598
Dedeo3 <yoannes.deo@ti.ukdw.ac.id>
2023-05-13 15:15:07 +0700

    Create SearchButtonKategori.java

---
+++ MadangSkuy/src/main/java/com/rplbo/madangskuy/SearchButtonKategori.java
@@ -0,0 +1,22 @@
+package com.rplbo.madangskuy;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SearchButtonKategori {
+    @FXML
+    public void backsearch(MouseEvent event) throws IOException {
+        Parent search = FXMLLoader.load(getClass().getResource("searching.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(search));
+        stage.show();
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
+    }
+}
