4acaa80e8d98fe77d032ad0bc8903c19f972a5c4
Dedeo3 <yoannes.deo@ti.ukdw.ac.id>
2023-05-13 15:15:22 +0700

    Update UserButtonKategori.java

--- MadangSkuy/src/main/java/com/rplbo/madangskuy/UserButtonKategori.java
+++ MadangSkuy/src/main/java/com/rplbo/madangskuy/UserButtonKategori.java
@@ -1,11 +1,15 @@
 package com.rplbo.madangskuy;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+
 
 import java.io.IOException;
 
@@ -19,6 +23,23 @@ public class UserButtonKategori {
         Stage stage = new Stage();
         stage.setScene(new Scene(mainPage));
         stage.show();
-        ((Node)(event.getSource())).getScene().getWindow().hide();
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
+    }
+
+    @FXML
+    private TextField search;
+
+
+    @FXML
+    public void search(ActionEvent event) throws IOException {
+        String keyword = search.getText().toLowerCase();
+        if (keyword.equals("sate")) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("searching.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+
     }
 }
