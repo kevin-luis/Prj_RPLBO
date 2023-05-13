33d89adca3cbaa4fdb47f11085c723a39e538e98
Dedeo3 <yoannes.deo@ti.ukdw.ac.id>
2023-05-13 15:15:57 +0700

    Update userSearchcontrol.java

--- MadangSkuy/src/main/java/com/rplbo/madangskuy/userSearchcontrol.java
+++ MadangSkuy/src/main/java/com/rplbo/madangskuy/userSearchcontrol.java
@@ -1,15 +1,41 @@
 package com.rplbo.madangskuy;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 public class userSearchcontrol {
 
+
+    public void klikSatepadang(MouseEvent event) throws IOException {
+        Parent kategoriPage = FXMLLoader.load(getClass().getResource("detail-page.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(kategoriPage));
+        stage.show();
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
+    }
+    public void backhome(MouseEvent event) throws IOException {
+        Parent kategoriPage = FXMLLoader.load(getClass().getResource("user-home.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(kategoriPage));
+        stage.show();
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
+    }
+
+    @FXML
+    public void kategori(ActionEvent event) throws IOException {
+        Parent kategoriPage = FXMLLoader.load(getClass().getResource("searchButtonKategori.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(kategoriPage));
+        stage.show();
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
+    }
 
 
 }
